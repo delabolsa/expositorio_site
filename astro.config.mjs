d7a71fa0184e4f0a1f7c0c4150e1b9f0bb83b81b
delabolsa <lvega21785@gmail.com>
2026-06-09 14:07:09 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,26 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'static', // en lugar de 'server'
-  adapter: cloudflare(),
- },
-
+  output: 'static',
+  vite: {
+    plugins: [tailwindcss()],
+  },
   markdown: {
     drafts: true,
     shikiConfig: {
       theme: "css-variables"
     }
   },
-
   shikiConfig: {
     wrap: true,
     skipInline: false,
     drafts: true
   },
-
-  site: 'https://yourwebsite.com',
+  site: 'https://expiatoriovoyeur.com',
   integrations: [sitemap(), mdx()],
   adapter: cloudflare()
 });
