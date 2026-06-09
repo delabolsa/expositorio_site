5463c9895b658009292be21611ff7b59a365a092
delabolsa <lvega21785@gmail.com>
2026-06-09 14:05:28 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,8 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-   plugins: [tailwindcss()],
+  output: 'static', // en lugar de 'server'
+  adapter: cloudflare(),
  },
 
   markdown: {
